7bb5e4b3d527a533a1d78ec57c417d6b294b3f88
Leandro Sgroi <lea220197@gmail.com>
2019-06-20 16:27:16 -0300

    datos usuario

---
+++ CuentaUsuario.java
@@ -0,0 +1,6 @@
+public class CuentaUsuario{
+	public static void main(String [] args){
+			System.out.println("Esta cuenta es del usuario Leandro Sgroi");
+			System.out.println("Saldo cuenta: $180.000");
+	}
+}
